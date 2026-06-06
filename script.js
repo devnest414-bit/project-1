8073b445c87594e3b1ab0b685ca2569504eb2296
devnest414-bit <devnest414@gmail.com>
2026-06-06 16:09:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var close = document.getElementById("fa1");
+var bars = document.getElementById("fa2");
+var MyUl = document.getElementById("MyUl");
+
+function showUL() {
+
+    MyUl.style.right = "-10px";
+
+}
+
+function hideUL(){
+    MyUl.style.right = "-220px";
+}
